dd9fda7c71d225592317a8f76df564780f1637a8
Brian Maggi <brianmaggi@gmail.com>
2021-12-07 15:26:34 -0800

    added key to modal

--- components/QuestionContainer/QuestionContainer.jsx
+++ components/QuestionContainer/QuestionContainer.jsx
@@ -82,7 +82,11 @@ const QuestionContainer = ({ question, index, setIsOpen, category }) => {
 				isOpen={toggle}
 				closeModal={closeModal}
 				title={`${category}  ${question.id}`}>
-				<Payload notes={question.notes} category={category} />
+				<Payload
+					notes={question.notes}
+					category={category}
+					key_question={question.key_question}
+				/>
 			</QuestionModal>
 		</div>
 	);
@@ -90,59 +94,68 @@ const QuestionContainer = ({ question, index, setIsOpen, category }) => {
 
 export default QuestionContainer;
 
-const Payload = ({ notes, category }) => {
+const Payload = ({ notes, category, key_question }) => {
 	return (
-		<div className='grid grid-cols-2'>
-			<div className='flex flex-col p-2 space-y-2'>
-				<div className='text-sm uppercase'>Category</div>
-				<div>
-					<select className='text-sm bg-gray-100 rounded'>
-						<option>{category}</option>
-					</select>
-				</div>
-				<div className='text-sm uppercase'>Question</div>
-				<div>{notes}</div>
-				<div className='text-sm uppercase'>Responses</div>
-				<div className='w-full p-8 text-center border border-gray-500 border-dashed'>
-					<div className='flex flex-row items-center justify-center w-full text-center'>
-						<div>Add Responses</div>
-						<PlusIcon className='inline-flex w-4 h-4 text-gray-400' />
+		<div>
+			<div className='grid grid-cols-2'>
+				<div className='flex flex-col p-2 space-y-2'>
+					<div>
+						{key_question ? (
+							<KeyIcon className='inline-flex w-8 h-8 text-indigo-500' />
+						) : (
+							<KeyIcon className='inline-flex w-8 h-8 text-gray-200' />
+						)}
+					</div>
+					<div className='text-sm uppercase'>Category</div>
+					<div>
+						<select className='text-sm bg-gray-100 rounded'>
+							<option>{category}</option>
+						</select>
+					</div>
+					<div className='text-sm uppercase'>Question</div>
+					<div>{notes}</div>
+					<div className='text-sm uppercase'>Responses</div>
+					<div className='w-full p-8 text-center border border-gray-500 border-dashed'>
+						<div className='flex flex-row items-center justify-center w-full text-center'>
+							<div>Add Responses</div>
+							<PlusIcon className='inline-flex w-4 h-4 text-gray-400' />
+						</div>
+					</div>
+					<div className='text-sm uppercase'>Links</div>
+					<div className='flex flex-col space-y-1 text-sm'>
+						<div className='p-2 space-x-3 text-white bg-gray-700 rounded-full in-flex'>
+							<span>Category Name</span>
+							<span className='p-1.5 bg-gray-900 rounded-full'>4</span>
+							<span className='p-1.5 bg-gray-900 rounded-full'>5</span>
+						</div>
+						<div className='p-2 text-white bg-gray-700 rounded-full in-flex'>
+							Category Name
+						</div>
+						<div className='flex flex-row px-2 py-2 space-x-2'>
+							<TrendingUpIcon className='w-5 h-5 text-gray-700' />
+							<div>Link to Another Question or Category</div>
+						</div>
 					</div>
 				</div>
-				<div className='text-sm uppercase'>Links</div>
-				<div className='flex flex-col space-y-1 text-sm'>
-					<div className='p-2 space-x-3 text-white bg-gray-700 rounded-full in-flex'>
-						<span>Category Name</span>
-						<span className='p-1.5 bg-gray-900 rounded-full'>4</span>
-						<span className='p-1.5 bg-gray-900 rounded-full'>5</span>
-					</div>
-					<div className='p-2 text-white bg-gray-700 rounded-full in-flex'>
-						Category Name
-					</div>
-					<div className='flex flex-row px-2 py-2 space-x-2'>
-						<TrendingUpIcon className='w-5 h-5 text-gray-700' />
-						<div>Link to Another Question or Category</div>
-					</div>
-				</div>
-			</div>
-			<div className='p-2'>
-				<div className='text-sm uppercase'>History</div>
-				<div>
-					<div className='flex flex-row items-center space-x-2'>
-						<div className='rounded-full w-[20px] h-[20px] bg-gray-300 inline-flex'></div>
-						<div>Brian Maggi edited 21m</div>
-					</div>
-					<div className='p-3 m-3 bg-gray-100 rounded'>
-						<div>{notes}</div>
-						<div className='mt-2 line-through'>{notes}</div>
-					</div>
-					<div className='flex flex-row items-center space-x-2'>
-						<div className='rounded-full w-[20px] h-[20px] bg-gray-300 inline-flex'></div>
-						<div>Jason Cline created 1d</div>
-					</div>
-					<div className='p-3 m-3 bg-gray-100 rounded'>
-						<div>{notes}</div>
-						<div className='mt-2 line-through'>{notes}</div>
+				<div className='p-2'>
+					<div className='text-sm uppercase'>History</div>
+					<div>
+						<div className='flex flex-row items-center space-x-2'>
+							<div className='rounded-full w-[20px] h-[20px] bg-gray-300 inline-flex'></div>
+							<div>Brian Maggi edited 21m</div>
+						</div>
+						<div className='p-3 m-3 bg-gray-100 rounded'>
+							<div>{notes}</div>
+							<div className='mt-2 line-through'>{notes}</div>
+						</div>
+						<div className='flex flex-row items-center space-x-2'>
+							<div className='rounded-full w-[20px] h-[20px] bg-gray-300 inline-flex'></div>
+							<div>Jason Cline created 1d</div>
+						</div>
+						<div className='p-3 m-3 bg-gray-100 rounded'>
+							<div>{notes}</div>
+							<div className='mt-2 line-through'>{notes}</div>
+						</div>
 					</div>
 				</div>
 			</div>
